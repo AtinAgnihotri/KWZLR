358e3191269d7ba788fc704ae1715e9c8ccbd45e
Atin Agnihotri <atinagnihotri@gmail.com>
2021-04-23 08:13:37 +0530

    Add Mock Logic

--- KWZLRv1.0/ViewController.swift
+++ KWZLRv1.0/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var questionText: UILabel!
+    @IBOutlet var progressBar: UIProgressView!
+    @IBOutlet var trueButton: UIButton!
+    @IBOutlet var falseButton: UIButton!
+    
+    let quiz = [
+        ["Four + Two Equals Six?", "True"],
+        ["Five - Three is greater than One?", "True"],
+        ["Eight + Seven is double of Nine?", "False"]
+    ]
+    
+    var questionNo = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        questionText.text = quiz[questionNo][0]
+        
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let answerSelected = sender.currentTitle!
+        let correctAnswer = quiz[questionNo][1]
+        checkAnswer(answerSelected, correctAnswer)
+        incrementQuestion()
+    }
+    
+    func checkAnswer (_ answerSelected : String, _ correctAnswer : String) {
+        let preString = "The Answer to Question No. \(questionNo + 1)"
+        if answerSelected == correctAnswer {
+            print(preString + " is correct")
+        } else {
+            print(preString + " is incorrect")
+        }
+    }
+    
+    func incrementQuestion() {
+        if questionNo < 2 {
+            questionNo += 1
+        }
+        questionText.text = quiz[questionNo][0]
+    }
+    
 }
 
